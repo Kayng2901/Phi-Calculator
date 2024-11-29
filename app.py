aa96dd49c6496cdd10e9c7856f6ebea3cf8fda2b
Khiem Nguyen <nguyengiakhiem2901@gmail.com>
2024-11-28 20:55:11 -0800

    Update host and port configuration for Render

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template
 from scipy.stats import norm
+import os
+
 
 app = Flask(__name__)
 
@@ -17,4 +19,6 @@ def laplace_table():
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if no port is specified
+    app.run(host='0.0.0.0', port=port)
+
